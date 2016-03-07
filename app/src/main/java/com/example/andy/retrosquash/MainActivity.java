e58dfb1fc089a021b54e3d03c28538b98f79e23a
Andy <andykeem@gmail.com>
2016-03-07 17:56:33 -0500

    ball moving logic initiated

--- app/src/main/java/com/example/andy/retrosquash/MainActivity.java
+++ app/src/main/java/com/example/andy/retrosquash/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.andy.retrosquash;
 
 import android.content.Context;
+import android.content.DialogInterface;
 import android.graphics.Bitmap;
 import android.graphics.Canvas;
 import android.graphics.Color;
@@ -9,6 +10,7 @@ import android.graphics.Point;
 import android.graphics.PointF;
 import android.graphics.Rect;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.util.DisplayMetrics;
 import android.util.Log;
@@ -21,6 +23,7 @@ public class MainActivity extends AppCompatActivity {
     private static final String TAG = "MainActivity";
     private SquashCourtView mCourtView;
     private Point mSize;
+    private boolean mShouldStart = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,14 +35,40 @@ public class MainActivity extends AppCompatActivity {
 
         mSize = new Point();
         this.getWindowManager().getDefaultDisplay().getSize(mSize);
+
+        this.showReadyDialog();
+    }
+
+    protected void showReadyDialog() {
+        new AlertDialog.Builder(MainActivity.this)
+                .setTitle("Retro Squash")
+                .setMessage("Are you ready?")
+                .setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        startGame();
+                    }
+                })
+                .setCancelable(false)
+                .show();
+    }
+
+    protected void startGame() {
+        mShouldStart = true;
+        mCourtView.moveBall();
     }
 
     private class SquashCourtView extends View {
         private static final int BAR_WIDTH = 200;
         private static final int BAR_HEIGHT = 25;
         private static final int BAR_BOTTOM_OFFSET = 250;
+        private static final float BALL_RADIUS = 20;
+        private static final int BALL_BOTTOM_OFFET = 280;
+
         private Canvas mCanvas;
         private Integer mBarXOffset;
+        private Float mBallX;
+        private Float mBallY;
 
         public SquashCourtView() {
             super(MainActivity.this);
@@ -50,6 +79,7 @@ public class MainActivity extends AppCompatActivity {
             super.onDraw(canvas);
             mCanvas = canvas;
             this.drawBar();
+            this.drawBall();
         }
 
         @Override
@@ -94,6 +124,26 @@ public class MainActivity extends AppCompatActivity {
             Paint paint = new Paint();
             paint.setColor(Color.BLACK);
             mCanvas.drawRect(bar, paint);
+        }
+
+        protected void drawBall() {
+            if (mBallX == null) {
+                mBallX = (mSize.x / 2) - (BALL_RADIUS / 2);
+            }
+            if (mBallY == null) {
+                mBallY = (float) mSize.y - BALL_BOTTOM_OFFET;
+            }
+            Paint paint = new Paint();
+            paint.setColor(Color.GREEN);
+            mCanvas.drawCircle(mBallX, mBallY, BALL_RADIUS, paint);
+        }
+
+        public void moveBall() {
+            Paint paint = new Paint();
+            paint.setColor(Color.GREEN);
+            mBallX += 10f;
+            mBallY -= 10f;
+            this.invalidate();
         }
     }
 }
