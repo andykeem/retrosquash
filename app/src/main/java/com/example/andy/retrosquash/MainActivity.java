fed50e01e300119f7366a461b29f7015c84a4219
Andy <andykeem@gmail.com>
2016-03-13 17:25:26 -0400

    MainActivity changed completely

--- app/src/main/java/com/example/andy/retrosquash/MainActivity.java
+++ app/src/main/java/com/example/andy/retrosquash/MainActivity.java
@@ -1,147 +1,220 @@
 package com.example.andy.retrosquash;
 
 import android.content.Context;
-import android.content.DialogInterface;
-import android.graphics.Bitmap;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Point;
-import android.graphics.PointF;
-import android.graphics.Rect;
 import android.os.Bundle;
-import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
-import android.util.DisplayMetrics;
 import android.util.Log;
 import android.view.MotionEvent;
-import android.view.View;
-import android.widget.ImageView;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = "MainActivity";
-    private SquashCourtView mCourtView;
-    private Point mSize;
-    private boolean mShouldStart = false;
+    private CourtView mCourtView;
+    private Point mScreenSize;
+    private int mScreenWidth;
+    private int mScreenHeight;
+
+    private Point mRacket;
+    private int mRacketWidth;
+    private int mRacketHeith;
+
+    private Point mBall;
+    private int mBallWidth;
+    private boolean mBallMoveDown;
+    private boolean mBallMoveLeft;
+    private boolean mBallMoveRight;
+    private boolean mBallMoveUp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 //        setContentView(R.layout.activity_main);
 
-        mCourtView = new SquashCourtView();
+        mCourtView = new CourtView(this);
         this.setContentView(mCourtView);
 
-        mSize = new Point();
-        this.getWindowManager().getDefaultDisplay().getSize(mSize);
+        mScreenSize = new Point();
+        this.getWindowManager().getDefaultDisplay().getSize(mScreenSize);
+        mScreenWidth = mScreenSize.x;
+        mScreenHeight = mScreenSize.y;
+//        Log.d(TAG, "screen size: " + mScreenSize);
 
-        this.showReadyDialog();
+        mRacket = new Point();
+        mRacket.x = mScreenWidth / 2;
+        mRacket.y = mScreenHeight - 200;
+        mRacketWidth = mScreenWidth / 6;
+        mRacketHeith = 10;
+
+        mBallWidth = mScreenWidth / 35;
+        mBall = new Point();
+        mBall.x = mScreenWidth / 2;
+        mBall.y = mBallWidth;
     }
 
-    protected void showReadyDialog() {
-        new AlertDialog.Builder(MainActivity.this)
-                .setTitle("Retro Squash")
-                .setMessage("Are you ready?")
-                .setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
-                        startGame();
-                    }
-                })
-                .setCancelable(false)
-                .show();
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mCourtView.resume();
     }
 
-    protected void startGame() {
-        mShouldStart = true;
-        mCourtView.moveBall();
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mCourtView.pause();
     }
 
-    private class SquashCourtView extends View {
-        private static final int BAR_WIDTH = 200;
-        private static final int BAR_HEIGHT = 25;
-        private static final int BAR_BOTTOM_OFFSET = 250;
-        private static final float BALL_RADIUS = 20;
-        private static final int BALL_BOTTOM_OFFET = 280;
+    private class CourtView extends SurfaceView implements Runnable {
 
+        private SurfaceHolder mHolder;
+        private Context mContext;
+        private Thread mThrd;
+        private boolean mPlaying;
         private Canvas mCanvas;
-        private Integer mBarXOffset;
-        private Float mBallX;
-        private Float mBallY;
+        private Paint mPaint;
+        private Random mRand;
 
-        public SquashCourtView() {
-            super(MainActivity.this);
-        }
+        public CourtView(Context context) {
+            super(context);
+            mContext = context;
+            mHolder = this.getHolder();
+            mPaint = new Paint();
 
-        @Override
-        protected void onDraw(Canvas canvas) {
-            super.onDraw(canvas);
-            mCanvas = canvas;
-            this.drawBar();
-            this.drawBall();
+            mBallMoveDown = true;
+            // get the random x angle
+            mRand = new Random();
+            int rand = mRand.nextInt(2);
+            Log.d(TAG, "random: " + rand);
+            switch (rand) {
+                case 0: // move to left
+                    mBallMoveLeft = true;
+                    mBallMoveRight = false;
+                    break;
+                case 1: // move to right
+                    mBallMoveLeft = false;
+                    mBallMoveRight = true;
+                    break;
+            }
         }
 
         @Override
         public boolean onTouchEvent(MotionEvent event) {
             super.onTouchEvent(event);
-            PointF point = new PointF(event.getX(), event.getY());
             switch (event.getAction()) {
                 case MotionEvent.ACTION_DOWN:
-
-                    break;
                 case MotionEvent.ACTION_MOVE:
-                    Log.d(TAG, "point: " + point.x);
-                    mBarXOffset = (int) point.x;
-                    this.invalidate();
-                    break;
-                case MotionEvent.ACTION_CANCEL:
-
-                    break;
-                case MotionEvent.ACTION_UP:
-
+                    mRacket.x = (int) event.getX();
                     break;
             }
             return true;
         }
 
-        protected void drawBar() {
-            int left = 0;
-            if (mBarXOffset == null) {
-                left = (mSize.x / 2 - (BAR_WIDTH / 2));
-            } else {
-                if (mBarXOffset > (mSize.x - BAR_WIDTH)) {
-                    left = mSize.x - BAR_WIDTH;
-                } else {
-                    left = mBarXOffset;
-                }
+        @Override
+        public void run() {
+            while (mPlaying) {
+                updateCourt();
+                drawCourt();
+                updateFPS();
             }
-            int top = mSize.y - BAR_BOTTOM_OFFSET;
-            int right = left + BAR_WIDTH;
-            int bottom = top + BAR_HEIGHT;
-            Rect bar = new Rect();
-            bar.set(left, top, right, bottom);
-            Paint paint = new Paint();
-            paint.setColor(Color.BLACK);
-            mCanvas.drawRect(bar, paint);
         }
 
-        protected void drawBall() {
-            if (mBallX == null) {
-                mBallX = (mSize.x / 2) - (BALL_RADIUS / 2);
+        protected void updateCourt() {
+//            Log.d(TAG, "updateCourt() called..");
+
+            // check side (x or y) collisions..
+            // if the ball touches the right side of the screen..
+            if ((mBall.x + mBallWidth) >= mScreenWidth) {
+                mBallMoveLeft = true;
+                mBallMoveRight = false;
             }
-            if (mBallY == null) {
-                mBallY = (float) mSize.y - BALL_BOTTOM_OFFET;
+
+            // if the ball touches the left side of the screen..
+            if (mBall.x <= 0) {
+                mBallMoveLeft = false;
+                mBallMoveRight = true;
             }
-            Paint paint = new Paint();
-            paint.setColor(Color.GREEN);
-            mCanvas.drawCircle(mBallX, mBallY, BALL_RADIUS, paint);
+
+            // move ball to top when it hits the bottom of the screen
+            if (mBall.y > mScreenHeight) {
+                mBall.y = mBallWidth;
+            }
+
+            // handle when ball hits the racket move ball up..
+            if ( ((mBall.y + mBallWidth) > (mRacket.y - (mRacketHeith / 2))) &&
+                    ((mBall.y + mBallWidth) < (mRacket.y + (mRacketHeith / 2))) &&
+                    ((mRacket.x - (mRacketWidth / 2)) < mBall.x) &&
+                    (mBall.x < (mRacket.x + (mRacketWidth / 2))) ) {
+                mBallMoveDown = false;
+                mBallMoveUp = true;
+            }
+
+            // if ball hits the top of the screen move ball down..
+            if (mBall.y <= 0) {
+                mBallMoveDown = true;
+                mBallMoveUp = false;
+            }
+
+            if (mBallMoveLeft) {
+                mBall.x -= 10;
+            }
+            if (mBallMoveRight) {
+                mBall.x += 10;
+            }
+            if (mBallMoveUp) {
+                mBall.y -= 10;
+            }
+            if (mBallMoveDown) {
+                mBall.y += 10;
+            }
         }
 
-        public void moveBall() {
-            mBallX += 10f;
-            mBallY -= 10f;
-            this.invalidate();
+        protected void drawCourt() {
+//            Log.d(TAG, "drawCourt() called..");
+
+            if (mHolder.getSurface().isValid()) {
+                mCanvas = mHolder.lockCanvas();
+                mCanvas.drawColor(Color.BLACK);
+                mPaint.setColor(Color.argb(255, 255, 255, 255));
+
+                // draw ball..
+                mCanvas.drawRect(mBall.x, mBall.y, (mBall.x + mBallWidth), (mBall.y + mBallWidth), mPaint);
+
+                // draw racket..
+                mCanvas.drawRect((mRacket.x - (mRacketWidth / 2)), (mRacket.y - (mRacketHeith / 2)),
+                        (mRacket.x + (mRacketWidth / 2)), (mRacket.y + (mRacketHeith / 2)), mPaint);
+
+                mHolder.unlockCanvasAndPost(mCanvas);
+            }
+        }
+
+        protected void updateFPS() {
+            try {
+                Thread.sleep(50);
+            } catch (InterruptedException ie) {
+                Log.e(TAG, ie.getMessage(), ie);
+            }
+        }
+
+        public void resume() {
+            mPlaying = true;
+            mThrd = new Thread(this);
+            mThrd.start();
+        }
+
+        public void pause() {
+            mPlaying = false;
+            try {
+                mThrd.join();
+            } catch (InterruptedException ie) {
+                Log.e(TAG, ie.getMessage(), ie);
+            }
         }
     }
 }
